23c5379e17537cd2425ce09e486bbedb507b3273
Uda25034 <C0A25034ed@edu.teu.ac.jp>
2026-04-21 14:55:03 +0900

    練習３

--- dodge_bomb.py
+++ dodge_bomb.py
@@ -1,16 +1,31 @@
 import os
 import sys
+import random
 import pygame as pg
 
 
 WIDTH, HEIGHT = 1100, 650
 DELTA = {
-    pg.K_UP: (0 , -5),  #上
-    pg.K_DOWN: (0 , +5),  #下
-    pg.K_LEFT:(-5 , 0),  #左
-    pg.K_RIGHT:(+5 , 0),  #右
-    }
+    pg.K_UP: (0 , -5),  # 上
+    pg.K_DOWN: (0 , +5),  # 下
+    pg.K_LEFT: (-5, 0),  # 左
+    pg.K_RIGHT: (+5, 0),  # 右
+}
 os.chdir(os.path.dirname(os.path.abspath(__file__)))
+
+
+def check_bound(rct: pg.Rect) -> tuple[bool, bool]:
+    """
+    引数で与えられたRectが画面内か画面外かを判定する関数
+    引数：こうかとんRectまたは爆弾Rect
+    戻り値：横方向，縦方向判定結果（True: 画面内，False: 画面外）
+    """
+    yoko, tate = True, True
+    if rct.left < 0 or WIDTH < rct.right:  # 横方向判定
+        yoko = False
+    if rct.top < 0 or HEIGHT < rct.bottom:  # 縦方向判定
+        tate = False
+    return yoko, tate
 
 
 def main():
@@ -20,6 +35,15 @@ def main():
     kk_img = pg.transform.rotozoom(pg.image.load("fig/3.png"), 0, 0.9)
     kk_rct = kk_img.get_rect()
     kk_rct.center = 300, 200
+
+    bb_img = pg.Surface((20, 20))  # 爆弾用の空のSurfaceを作る
+    pg.draw.circle(bb_img, (255, 0, 0), (10, 10), 10)  # 爆弾円を描く
+    bb_img.set_colorkey((0, 0, 0))  # 爆弾の黒い部分を透過させる
+    bb_rct = bb_img.get_rect()  # 爆弾Rectを取得する
+    bb_rct.centerx = random.randint(0, WIDTH)  # 爆弾の初期横座標を設定する
+    bb_rct.centery = random.randint(0, HEIGHT)  # 爆弾の初期縦座標を設定する
+    vx, vy = +5, +5  # 爆弾の速度
+
     clock = pg.time.Clock()
     tmr = 0
     while True:
@@ -30,20 +54,31 @@ def main():
 
         key_lst = pg.key.get_pressed()
         sum_mv = [0, 0]
-        #if key_lst[pg.K_UP]:
-        #    sum_mv[1] -= 5
-        #if key_lst[pg.K_DOWN]:
-        #    sum_mv[1] += 5
-        #if key_lst[pg.K_LEFT]:
-        #    sum_mv[0] -= 5
-        #if key_lst[pg.K_RIGHT]:
-        #    sum_mv[0] += 5
+        # if key_lst[pg.K_UP]:
+        #     sum_mv[1] -= 5
+        # if key_lst[pg.K_DOWN]:
+        #     sum_mv[1] += 5
+        # if key_lst[pg.K_LEFT]:
+        #     sum_mv[0] -= 5
+        # if key_lst[pg.K_RIGHT]:
+        #     sum_mv[0] += 5
         for key, mv in DELTA.items():
             if key_lst[key]:
                 sum_mv[0] += mv[0]
                 sum_mv[1] += mv[1]
         kk_rct.move_ip(sum_mv)
+        if check_bound(kk_rct) != (True, True):  # 画面外だったら
+            kk_rct.move_ip(-sum_mv[0], -sum_mv[1])
+ 
         screen.blit(kk_img, kk_rct)
+        bb_rct.move_ip(vx, vy)  # 爆弾を移動させる
+        yoko, tate = check_bound(bb_rct)
+        if not yoko:  # 横方向の判定
+            vx *= -1
+        if not tate:  # 縦方向の判定
+            vy *= -1
+
+        screen.blit(bb_img, bb_rct)  # 爆弾を表示させる
         pg.display.update()
         tmr += 1
         clock.tick(50)
